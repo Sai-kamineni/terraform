b55505a0d4ba44e562992b16fa78c80431caaf1e
Sai-kamineni <saikumar.dba10@gmail.com>
2024-11-25 20:27:17 +0530

    terrafrom

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -23,4 +23,15 @@ resource "aws_security_group" "allow_ssh_terraform" {
         Name = "allow_sshh"
     }
 
+
+}
+
+resource "aws_instance" "terraform" {
+
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = {
+        Name = "terraform"
+    }
 }
