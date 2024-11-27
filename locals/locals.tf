cff049dce462805968992597974cd7e68cc06f56
Sai-kamineni <saikumar.dba10@gmail.com>
2024-11-27 11:49:51 +0530

    locals

---
+++ locals/locals.tf
@@ -0,0 +1,6 @@
+locals {
+    domain_name  = "kamineni.site"
+    zone_id = "Z0144646135M9PQT22WCJ"
+    #instance_type = var.environment == "prod" ? "t3.medium" : "t3.micro"
+    # count.index will not work in locals
+}
