f3687ac6c045e574630b8c9e47c1580bab878ec5
Sai-kamineni <saikumar.dba10@gmail.com>
2024-11-27 11:18:36 +0530

    expense-1

---
+++ expense-1/variables.tf
@@ -0,0 +1,13 @@
+variable "instance_names" {
+  type        = list(string)
+  default     = ["mysql", "backend", "frontend"]
+  description = "description"
+}
+
+variable "zone_id" {
+  default = "Z0144646135M9PQT22WCJ"
+}
+
+variable "domain_name" {
+  default = "kamineni.site"
+}
